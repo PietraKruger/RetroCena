790bcaf4e5438e294237fd7422d3c0a4a1731921
PietraKruger <Pietrapikruger11@gmail.com>
2025-11-26 17:02:57 -0300

    tentando fazer crud

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+// Função para salvar a senha no localStorage
+salvarDados.addEventListener("click" , function() {
+    const nome = document.getElementById("nome").value;
+    console.log("Salvando dados...");
+});
+// const senha = campSenha.value;
+// localStorage.setItem("senha", senha);
+// alert("Senha salva com sucesso!");
