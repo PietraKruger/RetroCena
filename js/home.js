e90acee6d9d36cf03c1aba488acdeabdb8e937fc
Richard_htp24 <richard_pinheiro@estudante.sesisenai.org.br>
2026-02-11 02:36:27 -0300

    mudanças em home(css,html e js)

--- js/home.js
+++ js/home.js
@@ -1,2 +1,31 @@
 const teste = localStorage.getItem("usuario");
 console.log("teste", teste);
+
+//CARROSSEL//
+//seleção dos elementos
+const slides = document.querySelector('.slides');
+const imagens = document.querySelectorAll('.slides img');
+const btnEsq = document.querySelector('.esquerda');
+const btnDir = document.querySelector('.direita');
+
+let index = 0;
+//mostra a imagem correta
+function mostrarSlide() {
+  slides.style.transform = `translateX(${-index* 100}%)`;
+}
+//botão de navegação
+btnDir.addEventListener('click', () => {
+  index= (index + 1) % imagens.length;
+  mostrarSlide();
+});
+
+btnEsq.addEventListener('click', () => {
+  index = (index - 1 + imagens.length) % imagens.length;
+  mostrarSlide();
+});
+//carrossel automatico(opcional)
+setInterval(() => {
+  index = (index + 1) % imagens.length;
+  mostrarSlide();
+}, 3000);
+//CARROSSEL//
